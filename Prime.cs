3268f44ed83f02e042fdc14ed051880cd67e4efa
madhursuryawanshi <112289677+madhursuryawanshi@users.noreply.github.com>
2024-11-08 18:19:12 +0530

    Update Prime.cs

--- Prime.cs
+++ Prime.cs
@@ -18,22 +18,54 @@ private static bool isprime(int k)
         }
         return isprime;
     }
-public class Program
+
+class NewPrime : Prime
+{
+    public bool isprime(int number)  //override
     {
-        public static void Main(string[] args)
+        if (number <= 1)         //
         {
-            Prime prime = new Prime(); 
-            Console.WriteLine("Prime numbers up to 30:");
-            for (int i = 2; i <= 100; i++)
-            {
-                bool check = prime.IsPrime(i);
-                if (check)
-                {
-                    Console.WriteLine(i); 
-                }
-            }
-            int d = 12;
-            bool u = prime.IsPrime(d);
-            Console.WriteLine("prime Numner is " + u);           
+              return false
         }
+        // if (number == 2)  //true
+        //     {
+                  return true
+            }
+                if (number % 2 == 0)   // even number not find 
+                {
+
+                }  // false              // odd number not find      
+                        if (number % i==0 )
+                    {
+        for (int i=2;i<=40;i++)
+
+        // factor show to the output
+
+}   return true;
+// public bool isprime(int number)  // Same method name override
+//     {
+//         return isPrime()
+//     }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Prime prim = new Prime();
+        NewPrime P = new NewPrime();
+        Console.WriteLine("Prime numbers up to 30:");
+        for (int i = 2; i <= 100; i++)
+        {
+            bool check = prime.IsPrime(i);
+            if (check)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        int d = 12;
+        bool u = prime.IsPrime(d);
+        Console.WriteLine("prime Numner is " + u);
+        bool ad = NewPrime.Isprime(m);
+        Console.WriteLine("prime number is" + ad);
     }
+}
