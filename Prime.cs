19382f451dae0cbac288fe8bb5ec7ac14c6f86b5
madhursuryawanshi <112289677+madhursuryawanshi@users.noreply.github.com>
2024-11-11 09:44:07 +0530

    Update Prime.cs

--- Prime.cs
+++ Prime.cs
@@ -1,50 +1,51 @@
-class NewPrime : Prime
+using System;
+namespace HelloWorld
 {
-    public bool isprime(int number)  //override
+    public class Prime
     {
-        if (number <= 1)         //
+        public virtual bool IsPrime(int number)
         {
-             false;
-        }
-        // if (number == 2)  //true
-        //     {
-       }   return true
- if (number % 2 == 0)   // even number not find 
-   {
-        return false   
-                }  // false              // odd number not find      
-                        if (number % i==0 )
-                {
-        for (int i=2;i<=num;i++)
-      // factor show to the outp
-    } 
- return true;
-Console.WriteLine(i);
-}
-
-// public bool isprime(int number)  // Same method name override
-//     {
-//         return isPrime()
-//     }
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Prime prim = new Prime();
-        NewPrime P = new NewPrime();
-        Console.WriteLine("Prime numbers up to 30:");
-        for (int i = 2; i <= 100; i++)
-        {
-            bool check = prime.IsPrime(i);
-            if (check)
+            if (number <= 1)
+                return false;
+            for (int i = 2; i < number; i++)
             {
-                Console.WriteLine(i);
+                if (number % i == 0)
+                    return false;
             }
+            return true;
         }
-        int d = 12;
-        bool u = prime.IsPrime(d);
-        Console.WriteLine("prime Numner is " + u);        
-        bool ad = NewPrime.Isprime(m);
-        Console.WriteLine("prime number is" + ad);
+    }
+    public class NewPrime : Prime
+    {
+        public override bool IsPrime(int number)
+        {
+if (number <= 1)
+return false;
+if (number == 2)
+return true; 
+if (number % 2 == 0)
+return false;
+ for (int i = 3; i<= number; i ++)
+{
+ if (number % i == 0)
+return false;
+            }
+            return true;
+        }
+    }
+  // Main Program class
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Prime prime = new Prime();
+            NewPrime newPrime = new NewPrime();
+int d = 12;
+            bool isPrimeD = newPrime.IsPrime(d);
+            Console.WriteLine("Is d a prime number? isPrimeD");
+int m = 7;
+bool isPrimeM = newPrime.IsPrime(m);
+Console.WriteLine("Is m a prime number? isPrimeM");
+        }
     }
 }
